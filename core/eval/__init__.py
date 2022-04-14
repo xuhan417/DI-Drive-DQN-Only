b4b4a180c3aeab76e02a59a7eb8e6ea6db8f6be2
xuhan417 <hanxu417@g.ucla.edu>
2022-04-13 22:15:26 -0700

    delete extra files

--- core/eval/__init__.py
+++
@@ -1,6 +0,0 @@
-from core import SIMULATORS
-from .serial_evaluator import SerialEvaluator
-
-if 'carla' in SIMULATORS:
-    from .single_carla_evaluator import SingleCarlaEvaluator
-#     from .carla_benchmark_evaluator import CarlaBenchmarkEvaluator
--- core/utils/img_encoder/__init__.py
+++
@@ -1,9 +0,0 @@
-# from .vehicle_controller import VehiclePIDController, VehicleCapacController
-# from .pid_controller import PIDController, CustomController
-# from .mpc_controller import MPCController
-#
-# from .vae_model import VanillaVAE
-# from .model_wrappers import SteerNoiseWrapper
-# from .cilrs_model import CILRSModel
-
-from .bev_speed_model import BEVSpeedConvEncoder
